dad65c0b01bfbd19949b03c4def30642c9eedf29
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-03-09 20:20:03 +0100

    change the value

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod counter;
 
 fn main() {
     let counter = counter::Counter::new();
-    counter.run_threads(10);
+    counter.run_threads(100000);
     println!("Final counter value: {}", counter.get_value());
 }
