faea8a6b1e9301aaa427f11c50a5cb6a05aa4a03
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-04-19 15:32:11 +0200

    add tests

--- src/gui_clock.rs
+++ src/gui_clock.rs
@@ -97,7 +97,7 @@ impl App for ClockApp {
     }
 }
 
-fn polar_to_cartesian(center: egui::Pos2, length: f32, angle: f32) -> egui::Pos2 {
+pub fn polar_to_cartesian(center: egui::Pos2, length: f32, angle: f32) -> egui::Pos2 {
     egui::pos2(
         center.x + angle.sin() * length,
         center.y - angle.cos() * length,
