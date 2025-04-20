18a575bd54908bc5283dfa7d95a64b08c43f3c1f
Dariusz.Makarewicz <Dariusz.Makarewicz@gigaclear.com>
2025-04-20 14:39:41 +0200

    more tests

--- src/gui_clock/utils.rs
+++ src/gui_clock/utils.rs
@@ -8,6 +8,23 @@ pub fn polar_to_cartesian(center: egui::Pos2, length: f32, angle: f32) -> egui::
     )
 }
 
+use std::f32::consts::PI;
+
+pub struct ClockPid {
+    pub pid_second: f32,
+    pub pid_minute: f32,
+    pub pid_hour: f32,
+}
+
+impl ClockPid {
+    pub fn angles_in_radians(&self) -> (f32, f32, f32) {
+        let second_angle = (self.pid_second / 60.0) * 2.0 * PI;
+        let minute_angle = (self.pid_minute / 60.0) * 2.0 * PI;
+        let hour_angle = (self.pid_hour / 12.0) * 2.0 * PI;
+        (second_angle, minute_angle, hour_angle)
+    }
+}
+
 pub struct ClockAngles {
     pub second: f32,
     pub minute: f32,
