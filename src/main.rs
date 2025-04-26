c02d2940af842f93fbd77850726b2b5a20843223
Dariusz Makarewicz <dariusz.makarewicz@gigaclear.com>
2025-04-26 19:09:50 +0200

    set minimal allowed size

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use eframe::egui::{Vec2, ViewportBuilder};
 use rust_clock_gui::rust_clock_gui::ClockApp;
 
 fn main() -> eframe::Result<()> {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        viewport: ViewportBuilder::default().with_min_inner_size(Vec2::new(300.0, 300.0)),
+        ..Default::default()
+    };
     eframe::run_native(
         "ClockApp",
         options,
