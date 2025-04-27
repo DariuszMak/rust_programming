2c802fff8b1e75598b8f898687fd99342b86fb84
Dariusz Makarewicz <dariusz.makarewicz@gigaclear.com>
2025-04-27 08:45:36 +0200

    add month tests cases

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -226,6 +226,58 @@ mod tests {
     }
 
     #[test]
+    fn test_decompose_hours_minutes_seconds_millis_more_than_one_month_to_seconds_only() {
+        let milliseconds: i64 = 35 * 24 * 60 * 60 * 1000 + 22 * 60 * 1000 + 34 * 1000 + 329;
+
+        let duration = chrono::Duration::milliseconds(milliseconds);
+        let current_datetime = Local::now()
+            .date_naive()
+            .and_hms_opt(0, 0, 0)
+            .unwrap()
+            .and_local_timezone(Local)
+            .unwrap();
+
+        let expected_date = current_datetime + chrono::Duration::milliseconds(milliseconds as i64);
+        let components = decompose_duration(duration, current_datetime, true);
+
+        assert_eq!(components.year, expected_date.year());
+        assert_eq!(components.month, expected_date.month());
+        assert_eq!(current_datetime.month(), expected_date.month() - 2);
+        assert_eq!(components.day, expected_date.day());
+        assert_eq!(current_datetime.day(), expected_date.day() + 26);
+        assert_eq!(components.hours, 0);
+        assert_eq!(components.minutes, 0);
+        assert_eq!(components.seconds, 3025354);
+        assert_eq!(components.milliseconds, 329);
+    }
+
+    #[test]
+    fn test_decompose_hours_minutes_seconds_millis_more_than_one_month() {
+        let milliseconds: i64 = 35 * 24 * 60 * 60 * 1000 + 22 * 60 * 1000 + 34 * 1000 + 329;
+
+        let duration = chrono::Duration::milliseconds(milliseconds);
+        let current_datetime = Local::now()
+            .date_naive()
+            .and_hms_opt(0, 0, 0)
+            .unwrap()
+            .and_local_timezone(Local)
+            .unwrap();
+
+        let expected_date = current_datetime + chrono::Duration::milliseconds(milliseconds as i64);
+        let components = decompose_duration(duration, current_datetime, false);
+
+        assert_eq!(components.year, expected_date.year());
+        assert_eq!(components.month, expected_date.month());
+        assert_eq!(current_datetime.month(), expected_date.month() - 2);
+        assert_eq!(components.day, expected_date.day());
+        assert_eq!(current_datetime.day(), expected_date.day() + 26);
+        assert_eq!(components.hours, 0);
+        assert_eq!(components.minutes, 22);
+        assert_eq!(components.seconds, 34);
+        assert_eq!(components.milliseconds, 329);
+    }
+
+    #[test]
     fn test_midnight_clock_angles() {
         let time: Time = Time::new(1970, 1, 1, 0, 0, 0, 0);
         let angles = calculate_clock_angles(&time);
